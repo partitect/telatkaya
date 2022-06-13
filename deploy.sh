47fee3b18e1cfda105694bd5e5c279f41bddf429
partitect <telatkaya@yandex.com>
2022-06-13 23:46:31 +0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
-git subtree push --prefix dist origin gh-pages
+#!/bin/sh
+if [ -z "$1" ]
+then
+  echo "Which folder do you want to deploy to GitHub Pages?"
+  exit 1
+fi
+git subtree push --prefix $1 origin gh-pages
